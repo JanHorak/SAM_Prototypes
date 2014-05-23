c32ad3c5d3891d73c3b5e015b6d3841a1cd29bdc
Jan Horak <janhorak@gmx.de>
2014-05-23 19:48:44 +0200

    SAM #45. Improvement of MediaStorage and MediaFile added. Relations between Message and MediaFile added.

---
+++ SAM_MainServer/src/main/java/net/sam/server/entities/MediaFile.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sam.server.entities;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author janhorak
+ */
+@Entity
+public class MediaFile extends MediaStorage{
+    
+    
+    
+    
+}
